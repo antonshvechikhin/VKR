c66e340d54af062fe55e1b7e9ab3231c0f81c7d7
Anton Shvechikhin <antonkamski@gmail.com>
2024-03-18 22:21:09 +0400

    Added empty get_data_from_messengers method, updated index.html

--- SamaraProblemsClustering/urls.py
+++ SamaraProblemsClustering/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, get_data_from_messengers
 
 urlpatterns = [
     path('', index, name='index'),
+    path('get-data/', get_data_from_messengers, name='get_data_from_messengers'),
+
 ]
--- SamaraProblemsClustering/views.py
+++ SamaraProblemsClustering/views.py
@@ -1,8 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 
 def index(request):
-
     return render(request, 'index.html')
+
+
+def get_data_from_messengers(request):
+    if request.method == 'POST':
+        print(request.POST)
+        vk_token = request.POST.get('vkToken')
+        # Далее обработайте vk_token по вашему усмотрению
+        return HttpResponse("Данные VK Token получены: {}".format(vk_token))
+    else:
+        return HttpResponse("Метод запроса должен быть POST.")
