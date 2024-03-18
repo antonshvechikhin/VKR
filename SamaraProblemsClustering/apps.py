cf7002ad3bc4c837e1b1b319e8bbacd9e55c07fe
Anton Shvechikhin <antonkamski@gmail.com>
2024-03-18 21:33:51 +0400

    Added main view

---
+++ SamaraProblemsClustering/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SamaraproblemsclusteringConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SamaraProblemsClustering'
